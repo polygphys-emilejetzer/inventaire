8f540b1e01452ce2d0ba3724b5606f41f008df57
Émile Jetzer <emile.jetzer@polymtl.ca>
2022-07-12 17:34:01 -0400

    Ajout d'une fonction main pour permettre l'importation correcte

--- zotero/script_zotero.py
+++ zotero/script_zotero.py
@@ -13,30 +13,35 @@ import sqlalchemy
 
 from zotero import MigrationConfig, ZoteroItems
 
-journal = Path('~/zotero_a_inventaire.log').expanduser()
-chemin = Path('~/zotero_a_inventaire.cfg').expanduser()
-config = MigrationConfig(chemin)
+def main():
+        import logging
+        journal = Path('~/zotero_a_inventaire.log').expanduser()
+        chemin = Path('~/zotero_a_inventaire.cfg').expanduser()
+        config = MigrationConfig(chemin)
 
-zotero = config.get('zotero', 'adresse')
-inventaire2022 = config.get('inventaire2022', 'adresse')
-nom = config.get('inventaire2022', 'nom')
-mdp = config.get('inventaire2022', 'mdp')
+        zotero = config.get('zotero', 'adresse')
+        inventaire2022 = config.get('inventaire2022', 'adresse')
+        nom = config.get('inventaire2022', 'nom')
+        mdp = config.get('inventaire2022', 'mdp')
 
-inventaire2022 = inventaire2022.format(nom=nom, mdp=mdp)
-
-
-def enveloppe():
-    print(f'Mise à jour {datetime.now()}...')
-    try:
-        bd = ZoteroItems(zotero, inventaire2022)
-        bd.charger()
-    except (Exception, sqlite3.OperationalError, sqlalchemy.exc.OperationalError):
-        with journal.open() as f:
-            traceback.print_exc(file=f)
+        inventaire2022 = inventaire2022.format(nom=nom, mdp=mdp)
 
 
-schedule.every(10).minutes.do(enveloppe)
-print('On commence...')
-while True:
-    schedule.run_pending()
-    time.sleep(10)
+        def enveloppe():
+            logging.info(f'Mise à jour {datetime.now()}...')
+            try:
+                bd = ZoteroItems(zotero, inventaire2022)
+                bd.charger()
+            except (Exception, sqlite3.OperationalError, sqlalchemy.exc.OperationalError):
+                with journal.open() as f:
+                    traceback.print_exc(file=f)
+
+
+        schedule.every(10).minutes.do(enveloppe)
+        logging.info('On commence...')
+        while True:
+            schedule.run_pending()
+            time.sleep(10)
+
+if __name__ == '__main__':
+    main()
